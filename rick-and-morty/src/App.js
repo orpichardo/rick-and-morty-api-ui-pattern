e6cb6946df65d65d0b73d82eb49349f2e3461f5e
orpichardo <orlandopichardo809@gmail.com>
2023-01-11 15:47:27 -0500

    Project 98% Done

--- rick-and-morty/src/App.js
+++ rick-and-morty/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 
 function App() {
   const [characters, setCharacters] = useState([])
+  const [character, setCharacter] = useState({})
+  let [modalToggle, setModalToggle] = useState(false)
 
   useEffect(() => {
     apiCall()
@@ -15,20 +17,44 @@ function App() {
       .then((data) => setCharacters(data.results))
   }
 
+  function display() {
+    setModalToggle(prev => !prev)
+  }
+
+  function handleClick(charInfo) {
+    setCharacter(charInfo)
+    display()
+  }
+
   return (
     <div className="App">
       <Nav />
-      {characters.map((character) => (
-        <div>
-          <img src = {character.image} />
-          <h3>{character.name}</h3>
-          <p>Status - {character.status}</p>
-          <p>Species - {character.species}</p>
-          <p>Gender - {character.gender}</p>
-          <p>Origin - {character.origin.name}</p>
-          <p>Location - {character.location.name}</p>
+      <div className="characters-list">
+        {characters.map((character, index) => (
+          <div className="character-container" onClick={() => handleClick(character)} key={index}>
+            <img src={character.image} />
+            <h3>{character.name}</h3>
+          </div>
+        ))}
+      </div>
+      {modalToggle ?
+        <div className="modal">
+          <div className="modal-content">
+            <div className="info">
+                <h1>{character.name}</h1>
+            <p>Status - {character.status}</p>
+            <p>Species - {character.species}</p>
+            <p>Gender - {character.gender}</p>
+            <p>Origin - {character.origin.name}</p>
+            <p>Location - {character.location.name}</p>
+              <button onClick={display}>x</button>
+              </div>
         </div>
-      ))}
+          </div>
+        
+        :
+        null
+      }
     </div>
   );
 }
